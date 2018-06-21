43ce9ee79f07c91b320d746e5f1a6bd1fea384c9
lucio <lucio0314@163.com>
2018-06-21 15:39:34 +0800

    增加创建密码dialog

--- app/src/main/java/com/wallet/crypto/ui/dialog/CreatePwdDialog.kt
+++ app/src/main/java/com/wallet/crypto/ui/dialog/CreatePwdDialog.kt
@@ -2,6 +2,8 @@ package com.wallet.crypto.ui.dialog
 
 import android.view.LayoutInflater
 import com.wallet.crypto.R
+import com.wallet.crypto.ext.setTextChangeListener
+import kotlinx.android.synthetic.main.layout_dialog_create_pwd.*
 
 /**
  * @Description
@@ -10,10 +12,12 @@ import com.wallet.crypto.R
  * @Date 21/06/2018 3:04 PM
  * @Version
  */
-class CreatePwdDialog :BaseDialogFragment() {
+class CreatePwdDialog : BaseDialogFragment() {
 
     override fun build(initialBuilder: Builder): Builder {
-        val view = LayoutInflater.from(context).inflate(R.layout.layout_dialog_backup, null)
+        val view = LayoutInflater.from(context).inflate(R.layout.layout_dialog_create_pwd, null)
+        password.text.toString()
+        password.setTextChangeListener { positiveListeners?.onPositiveButtonClicked(0, it) }
         return initialBuilder.setView(view)
     }
 }
